603b063cb91deabf7ef9bee2b9e7c81b70344492
erwindavid <36957018+erwindavid@users.noreply.github.com>
2023-04-22 12:20:53 -0400

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,53 @@
 import * as React from 'react';
 import './style.css';
+import { useState, useEffect, useRef } from 'react'
 
 export default function App() {
+    const [scrolls, setScrolls] = useState([
+      {element:"lorem", scrollTop:0},
+      {element:"ipsum", scrollTop:0}
+  ])
+
+  function scrollElement(element, event) {
+    const newScrolls = scrolls.map(e=>{return {...e}})
+    newScrolls.map(e=>{
+        if (e.element==element) {
+            e.scrollTop = Math.round(event.target.scrollTop)+1
+        }
+    })
+    setScrolls(newScrolls)                    
+  }
+
+  function Element(props) {
+    console.log('Element')
+    let elementRef = useRef()
+
+    useEffect(()=>{
+      if (elementRef) elementRef.current.scrollTop = props.scrollTop
+    },[elementRef, props.scrollTop])
+
+    return (
+      <div >
+          <div>{props.element+":"+props.scrollTop}</div>
+          <div 
+          ref={elementRef}
+          id={props.element}
+          className="els"
+          onScroll={()=>{props.scrollElement(props.element, event)}}
+          style={{height:"200px", width:"200px", overflow: "scroll", border:"1px solid black"}} 
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porttitor vulputate consequat. Suspendisse elementum massa at lectus venenatis, vel lacinia risus consectetur. Morbi eget vestibulum ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et placerat leo, sit amet rhoncus tellus. Donec eget felis sed velit mollis mattis in id sem.</div>
+      </div>    
+    )
+  }
+
   return (
-    <div>
-      <h1>Hello Excerd</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+      <div>
+        {scrolls.map(e=><Element
+            key={e.element}
+            element={e.element}
+            scrollTop={e.scrollTop}
+            scrollElement={scrollElement} />)}
+      </div>
+  )
+
 }
